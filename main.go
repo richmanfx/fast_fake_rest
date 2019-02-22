3cd33a43b34cf70398a3d153a7cdd71b7a0594ec
Александр Ящук <a.yashuk@pflb.ru>
2019-02-22 15:58:52 +0300

    Logger connected

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 /****************************************************************************/
-/*					Program to use QA as the rest-stub. 					*/
+/*					Program to use in QA as the rest-stub. 					*/
 /*                                                                          */
-/*					Russia, Moscow, Zoer aka R5AM, 2019.                    */
+/*					 Russia, Moscow, Zoer aka R5AM, 2019.                   */
 /*                                                                          */
 /****************************************************************************/
 
 package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"os"
+)
 
 func main() {
 
@@ -14,5 +19,20 @@ func main() {
 		configFileName = "config.yaml"
 	)
 
+	// Set logging parameters
+	SetLog(log.DebugLevel)
 
+	// Full configuration file name
+	fullConfigFileName := configDirName + "/" + configFileName
+	log.Debugf("Full config file name: %s", fullConfigFileName)
+}
+
+/* Set logging parameters */
+func SetLog(debugLevel log.Level) {
+	log.SetOutput(os.Stdout)
+	customFormatter := new(log.TextFormatter)
+	customFormatter.TimestampFormat = "2006/01/02 15:04:05"
+	log.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
+	log.SetLevel(debugLevel)
 }
