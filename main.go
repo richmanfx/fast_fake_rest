baa2710b78e94f15e4a383eba93c28f96e9e97b7
Александр Ящук <a.yashuk@pflb.ru>
2019-02-22 15:47:38 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+/****************************************************************************/
+/*					Program to use QA as the rest-stub. 					*/
+/*                                                                          */
+/*					Russia, Moscow, Zoer aka R5AM, 2019.                    */
+/*                                                                          */
+/****************************************************************************/
+
+package main
+
+func main() {
+
+	const (
+		configDirName  = "/usr/local/etc/fast_fake_rest"
+		configFileName = "config.yaml"
+	)
+
+
+}
